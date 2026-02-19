3cace797544cf08f9eee8cb1c177d6246a50efa3
Anvin <gooseinteandi@gmail.com>
2026-02-19 09:33:00 +0530

    fix(electron): correct production path to index.html

--- electron/main.ts
+++ electron/main.ts
@@ -40,7 +40,7 @@ function createWindow() {
     mainWindow.loadURL('http://localhost:5173')
     mainWindow.webContents.openDevTools({ mode: 'detach' })
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
+    mainWindow.loadFile(path.join(__dirname, '../../dist/index.html'))
   }
 
   mainWindow.on('closed', () => {
